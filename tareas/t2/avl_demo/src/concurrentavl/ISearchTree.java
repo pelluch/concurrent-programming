af33a4dc71f1fc19c5aeac950989d43a005d7f6a
Pablo Lluch <pablo.lluch@gmail.com>
2013-09-15 06:18:57 -0300

    Add demo avl tree with tests

---
+++ tareas/t2/avl_demo/src/concurrentavl/ISearchTree.java
@@ -0,0 +1,8 @@
+package concurrentavl;
+
+public interface ISearchTree {
+	public boolean find(int value);
+	public void insert(int value);
+	public void delete(int value);
+
+}
---
+++ tareas/t2/avl_demo/src/concurrentavl/Operation.java
@@ -0,0 +1,18 @@
+package concurrentavl;
+
+public enum Operation {
+    Insert("INSERT"), Find("FIND"), Delete("DELETE"), SynchroInsert(
+            "SYNCHROINSERT"), SynchroFind("SYNCHROFIND"), SynchroDelete(
+            "SYNCHRODELETE");
+
+    private String mData;
+
+    private Operation(String data) {
+        mData = data;
+    }
+
+    @Override
+    public String toString() {
+        return mData;
+    };
+}
